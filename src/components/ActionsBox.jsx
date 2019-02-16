ad5dfeae48a836639c10046e5f67dc5fd27ca347
Jay Wilson <jwilso48@masonlive.gmu.edu>
2019-02-16 10:28:34 -0500

    set up boilerplate

---
+++ src/components/ActionsBox.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class ActionsBox extends Component {
+  render() {
+    return (
+      null
+    );
+  }
+}
+
+export default ActionsBox;
---
+++ src/components/StatsBox.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class StatsBox extends Component {
+  render() {
+    return (
+      null
+    );
+  }
+}
+
+export default StatsBox;
