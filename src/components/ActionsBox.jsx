80e15f03ea65f667db3c1cb73575c67a8ce88f89
Jay Wilson <jwilso48@masonlive.gmu.edu>
2019-02-17 06:57:45 -0500

    reformatted actionsbox state

--- src/components/ActionsBox.jsx
+++ src/components/ActionsBox.jsx
@@ -5,10 +5,36 @@ import { getTransactionsByDay } from "../TransactionsByDay";
 class ActionsBox extends Component {
   constructor(props) {
     super(props);
+
     this.state = {
-      selected: "spend",
-      actions: []
+      currentTab: "spend",
+      recentActions: [],
+      actions: {
+        spend: [],
+        save: [],
+        borrow: [
+          { name: "Take out a payday loan", cost: -500 },
+          { name: "Neglect to pay off your student loans", cost: -500}
+        ],
+        invest: [
+          { name: "Invest in an untested cryptocurrency", cost: -300},
+          { name: "Purchase random stocks on Robinhood", cost: -500}
+        ]
+      }
     }
+
+    // this.props.actions = {
+    //   spend: [],
+    //   save: [],
+    //   borrow: [
+    //     { name: "Take out a payday loan", cost: -500 },
+    //     { name: "Neglect to pay off your student loans", cost: -500}
+    //   ],
+    //   invest: [
+    //     { name: "Invest in an untested cryptocurrency", cost: -300},
+    //     { name: "Purchase random stocks on Robinhood", cost: -500}
+    //   ]
+    // };
   }
 
   componentDidMount() {
@@ -19,8 +45,9 @@ class ActionsBox extends Component {
         this.setState({ checking: account.account.balance });
       }
     });
-    let actions = getTransactionsByDay(this.props);
-    this.setState({ actions: actions });
+    let spendActions = getTransactionsByDay(this.props);
+    // this.state.actions.spend = spendActions;
+    this.setState({ actions: { spend: spendActions } });
     console.log(this.state);
   }
 
@@ -31,12 +58,13 @@ class ActionsBox extends Component {
     return "$" + cents / 100.0;
   }
 
-  handleClick() {
-    console.log("click!");
+  handleClick(id) {
+    console.log("clicked on " + id);
+    this.setState({ currentTab: id });
   }
 
   render() {
-    const actions = this.state.actions.map((action, index) => {
+    const actions = this.state.actions.spend.map((action, index) => {
       return (
         <li key={index}>
           {action.name}: {this.formatCurrency(action.cost)}
@@ -45,20 +73,12 @@ class ActionsBox extends Component {
     });
     return (
       <div className="actions-box">
-        <div className="tab-bar">
-          <h3 className="tab" id="spend" onClick={this.handleClick()}>
-            Spend
-          </h3>
-          <h3 className="tab" id="save" onClick={this.handleClick()}>
-            Save
-          </h3>
-          <h3 className="tab" id="borrow" onClick={this.handleClick()}>
-            Borrow
-          </h3>
-          <h3 className="tab" id="invest" onClick={this.handleClick(this.id)}>
-            Invest
-          </h3>
-        </div>
+        <ul className="tab-bar">
+          <li><a href="#" id="tab-spend" onClick={() => this.handleClick("spend")}>Spend</a></li>
+          <li><a href="#" id="tab-save" onClick={() => this.handleClick("save")}>Save</a></li>
+          <li><a href="#" id="tab-borrow" onClick={() => this.handleClick("borrow")}>Borrow</a></li>
+          <li><a href="#" id="tab-invest" onClick={() => this.handleClick("invest")}>Invest</a></li>
+        </ul>
         <div id="actions-list">
           <ul>{actions}</ul>
         </div>
@@ -66,46 +86,5 @@ class ActionsBox extends Component {
     );
   }
 }
-
-// class ActionsList extends Component {
-//   componentDidMount() {
-//     this.props.accounts.forEach(account => {
-//       if (account.account.type === "Savings") {
-//         this.setState({ savings: account.account.balance });
-//       } else {
-//         this.setState({ checking: account.account.balance });
-//       }
-//     });
-//     let actions = getTransactionsByDay(this.props);
-//     this.setState({ actions: actions });
-//     console.log(this.state);
-//     this.calculateDebt(actions);
-//   }
-
-//   render() {
-//     const actions = this.state.actions.map((action, index) => {
-//       return (
-//         <li key={index}>
-//           {action.name}: {this.formatCurrency(action.cost)}
-//         </li>
-//       );
-//     });
-//     return (
-//       <div id="RecentActions">
-//         <ul>{actions}</ul>
-//       </div>
-//     );
-//   }
-// }
-// class ActionsBox extends Component {
-//   render() {
-//     return (
-//       <div className="ActionsBox">
-//         <TabBar />
-//         <ActionsList />
-//       </div>
-//     );
-//   }
-// }
 
 export default ActionsBox;
