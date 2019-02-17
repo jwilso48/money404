78ecf98982ba627e15221800e1b9a964f14c1a71
Christopher Edwards <cedwards036@gmail.com>
2019-02-16 19:16:14 -0500

    for the merge

---
+++ src/graphData.js
@@ -0,0 +1,22 @@
+/**
+ * Transform a customer data object into data for a two-line graph
+ * @param {object} customerData - the customer data to summarize
+ */
+function getGraphData(customerData) {
+    let graphData = [];
+    let startingAssets = 0;
+    let startingDebts = 0;
+    console.log(customerData.accounts)
+    customerData.accounts.forEach(account => {
+        startingAssets += account.account.balance;
+        account.loans.forEach(loan => {
+            startingDebts += loan.amount;
+        });
+    });
+    console.log(startingDebts);
+    return graphData;
+}
+
+export {
+    getGraphData
+}
